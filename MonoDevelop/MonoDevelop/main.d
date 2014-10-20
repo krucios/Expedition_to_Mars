213acc08bf6d0a703d4dda8387b4280900bfefa5
Andrei Paznak <krucios@mail.ru>
2014-10-20 00:31:34 +0000

    This commit written within MonoDevelop (:

---
+++ MonoDevelop/MonoDevelop/main.d
@@ -0,0 +1,13 @@
+﻿module main;
+
+import std.stdio;
+
+void main(string[] args)
+{
+	for (auto i = 0; i < 10; ++i)
+		writeln("Hello World!");
+	
+	// Lets the user press <Return> before program returns
+	stdin.readln();
+}
+
