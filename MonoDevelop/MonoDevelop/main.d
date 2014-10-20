c358d140109981e3320feffad90c565e11f67cc8
Andrei Paznak <krucios@mail.ru>
2014-10-20 00:34:18 +0000

    >-)))):>

--- MonoDevelop/MonoDevelop/main.d
+++ MonoDevelop/MonoDevelop/main.d
@@ -6,7 +6,7 @@ void main(string[] args)
 {
 	for (auto i = 0; i < 10; ++i)
 		writeln("Hello World!");
-	
+	writeln("Some changes");
 	// Lets the user press <Return> before program returns
 	stdin.readln();
 }
