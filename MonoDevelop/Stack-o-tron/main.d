1e6593e5d4b682e604266a110dc7cc937fea9ac3
Andrei Paznak <krucios@mail.ru>
2014-10-21 00:11:22 +0000

    Stck-o-tron

---
+++ MonoDevelop/Stack-o-tron/main.d
@@ -0,0 +1,17 @@
+﻿module main;
+
+import std.stdio, stack;
+
+static immutable COUNT_OF_BYTES = 1024;
+
+static int[] memory = new int[COUNT_OF_BYTES / int.sizeof];
+static int bias;
+static int top;
+
+void main(string[] args)
+{
+	int cur_word;
+	writeln("Stack-o-tron glad to see you! \n");
+	readln();
+}
+
---
+++ MonoDevelop/Stack-o-tron/stack.d
@@ -0,0 +1,29 @@
+﻿module stack;
+
+class Stack
+{
+	int[] stack_mem;
+	int count;
+
+public:
+	this()
+	{
+		// Constructor code
+	}
+
+	bool isEmpty()
+	{
+		// TODO
+		return true;
+	};
+	void push(int n)
+	{
+		// TODO
+	};
+	int pop()
+	{
+		// TODO
+		return 0;
+	};
+}
+
