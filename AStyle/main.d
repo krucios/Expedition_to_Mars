ec6e0e4f939105cf9dfbcd17ce173292698752e5
Andrei Pazniak <krucios@mail.ru>
2014-11-01 20:40:41 +0000

    Only for training

--- AStyle/main.d
+++ AStyle/main.d
@@ -1,18 +1,37 @@
 ﻿module main;
 
-import std.stdio, std.conv, std.string, std.algorithm;
+import std.stdio, std.conv, std.string, std.algorithm, std.file;
 
-string[] separators = [" ", ",", ".", "-", ":"]; 
+int[] separators = [to!int(';'), to!int('}')]; 
 
 void main(string[] args)
 {
+	string text = readText("../../Files/input.txt");
+
 	writeln ("** Start lexical analyze **");
-	foreach (line; stdin.byLine())
+	string buffer;
+	bool is_quotes_open = false;
+	foreach (symbol; text)
 	{
-		foreach (symbol; line)
+		if (symbol != '\n' && symbol != '\t')
+			buffer ~= symbol;
+		else
+			buffer ~= ' ';
+		if (symbol == '"' || symbol == '\'')
+			is_quotes_open = !is_quotes_open;
+		if (!is_quotes_open)
 		{
-			writeln (symbol);
+			foreach (sep; separators)
+			{
+				if (symbol == sep)
+				{
+					buffer = strip(buffer);
+					buffer ~= "[" ~ to!string(sep) ~ "]";
+					writeln (buffer);
+					buffer.length = 0;
+					break;
+				}
+			}
 		}
 	}
 }
-
