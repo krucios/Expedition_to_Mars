31b01d29e40a8e71436d117a482b2c116e7a0e86
Andrei Pazniak <krucios@mail.ru>
2014-11-01 19:09:24 +0000

    First step

---
+++ AStyle/main.d
@@ -0,0 +1,18 @@
+﻿module main;
+
+import std.stdio, std.conv, std.string, std.algorithm;
+
+string[] separators = [" ", ",", ".", "-", ":"]; 
+
+void main(string[] args)
+{
+	writeln ("** Start lexical analyze **");
+	foreach (line; stdin.byLine())
+	{
+		foreach (symbol; line)
+		{
+			writeln (symbol);
+		}
+	}
+}
+
